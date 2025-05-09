73974beb32d77d53c80b55e7214b6cc66db1af39
takuyaetwifo <59362248+takuyaetwifo@users.noreply.github.com>
2025-05-09 21:59:51 +0900

    Create model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+
+class ChatLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_input = db.Column(db.Text, nullable=False)
+    gpt_response = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime)
