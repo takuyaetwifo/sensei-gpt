46885e3a8659feb07b008db15cd62a0c4da2f8b8
takuyaetwifo <59362248+takuyaetwifo@users.noreply.github.com>
2025-05-10 17:57:25 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 import os
 
+#class Config:
+#    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
+#    SECRET_KEY = "your_secret_key"
+
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "postgresql://postgres:2933@localhost:5432/sensei_gpt"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "your_secret_key"
+
+    
+#class Config:
+#    SECRET_KEY = os.environ.get("SECRET_KEY", "devkey")
+#    SQLALCHEMY_DATABASE_URI = "sqlite:///local.db"
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
